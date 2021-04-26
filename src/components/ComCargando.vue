877067c699fe23c50d7cf832374dc86349fb1bf8
alberto <albertocarrascofernandez@hotmail.com>
2021-04-26 21:04:26 +0200

    26-04

---
+++ src/components/ComCargando.vue
@@ -0,0 +1,18 @@
+ <template>
+    <v-row
+    class="fill-height ma-0"
+    align="center"
+    justify="center">
+         <v-progress-circular
+        indeterminate
+        color="color2">
+        </v-progress-circular>
+    </v-row>
+</template>
+
+<script>
+export default {
+    name: 'comCargando',
+   
+    }
+</script>
--- src/views/Arte.vue
+++ src/views/Arte.vue
@@ -4,21 +4,14 @@
             <h1  class="color2--text d-flex justify-center text-center titulo my-6">Galeria De Fotos Dinámica</h1>
         </v-container>
 
-        <v-sheet class="mx-auto d-flex justify-center" elevation="8" >
-      <v-slide-group  class="pa-4" show-arrows center-active>
-        <v-slide-item  v-for="(item,index) in cuadros" :key="index"
-        v-slot="{ active, toggle }">
-              <v-hover v-slot="{ hover }" >
-          <v-card
-            :color="active ? 'primary' : 'grey lighten-1'"
-            class="ma-4"
-          height="100"
-          width="200"
-            @click="toggle"
-            :to="{name:'arte', params: {page: item.id}}"
-          >
-          
-             
+        <v-sheet class="mx-auto d-flex justify-center">
+            <v-slide-group  class="pa-4" show-arrows center-active >
+                <v-slide-item  v-for="(item,index) in cuadros" :key="index"
+                v-slot="{  toggle }">
+                    <v-hover v-slot="{ hover }" >
+                        <v-card class="ma-4" height="100" color="black"
+                        width="200" @click="toggle"
+                        :to="{name:'arte', params: {page: item.id}}">
                             <v-img  aspect-ratio="2"
                             :src="item.foto">
                                 <v-expand-transition>
@@ -29,26 +22,24 @@
                                             Entrar
                                     </div>
                                 </v-expand-transition>
+                                <!--cargando-->
+                                <template v-slot:placeholder>
+                                  <comcargando></comcargando>
+                                </template>
+                                <!--cargando-->
                             </v-img>
-             <v-card-text class="py-8 botonEntrar text-capitalize  text-caption 
-                            text-sm-subtitle-1 text-md-h6 text-lg-h4 text-xl-h3">
-                    {{item.titulo}}
-             </v-card-text>
-    
-            
 
-          </v-card>
-            </v-hover>
-        </v-slide-item>
-      </v-slide-group>
-  
-    
-    </v-sheet>
-      
- 
-     
-    
-        <comCuadro :cuadros="cuadros"/>
+                            <v-card-text class="py-8 botonEntrar text-capitalize  text-caption 
+                            text-sm-subtitle-1 text-md-h6 text-lg-h4 text-xl-h3">
+                                {{item.titulo}}
+                            </v-card-text>
+                        </v-card>
+                    </v-hover>
+                </v-slide-item>
+            </v-slide-group>
+        </v-sheet>
+
+        <comCuadro :cuadros="cuadros"></comCuadro> 
 
     </div>
 </template>
@@ -73,62 +64,85 @@
 </style>
 
 <script>
-import comCuadro from "@/components/Cuadros/ComCuadros.vue"
+import comcargando from "@/components/ComCargando.vue"
+import comCuadro from "@/components/ComCuadros.vue"
+
 
 export default {
+    name: 'Arte',
     components:{
-       comCuadro
+       comcargando, comCuadro
     },
-    data(){
-        return{
-        
-
+    data:()=>({
+  
     cuadros:[
+          {
+            id:'bosquesinfin',
+            titulo:'Bosque Sin Fin',
+            foto: require('@/assets/cuadros/bosque/1.png'),
+            galeria: [
+                        require('@/assets/cuadros/bosque/1.png'),
+                    ],
+        },
         {
             id:'dialluvioso',
             titulo:'Dia Lluvioso',
             foto: require('@/assets/cuadros/diaLluvioso/3.png'),
+            galeria: [
+                        require('@/assets/cuadros/diaLluvioso/1.png'),
+                        require('@/assets/cuadros/diaLluvioso/2.png'),
+                        require('@/assets/cuadros/diaLluvioso/3.png'),
+                    ],
         },
          {
             id:'retratoana',
             titulo:'Retrato Ana',
             foto: require('@/assets/cuadros/ana/4.png'),
+            galeria: [
+                        require('@/assets/cuadros/ana/1.png'),
+                        require('@/assets/cuadros/ana/2.png'),
+                        require('@/assets/cuadros/ana/3.png'),
+                        require('@/assets/cuadros/ana/4.png'),
+                        require('@/assets/cuadros/ana/5.png'),
+                    ],
         },
          {
             id:'lageisha',
             titulo:'La Geisha',
             foto: require('@/assets/cuadros/laGeisha/2.png'),
+            galeria: [
+                        require('@/assets/cuadros/laGeisha/1.png'),
+                        require('@/assets/cuadros/laGeisha/2.png'),
+                    ],
         },
          {
             id:'torrepaterna',
             titulo:'Torre Paterna',
             foto: require('@/assets/cuadros/torrePaterna/3.png'),
+            galeria: [
+                        require('@/assets/cuadros/torrePaterna/1.png'),
+                        require('@/assets/cuadros/torrePaterna/2.png'),
+                        require('@/assets/cuadros/torrePaterna/3.png'),
+                    ],
         },
          {
             id:'libros',
             titulo:'Libros',
             foto: require('@/assets/cuadros/libros/1.png'),
+            galeria: [
+                        require('@/assets/cuadros/libros/1.png'),
+                    ],
         },
          {
             id:'cesta',
             titulo:'Cesta',
             foto: require('@/assets/cuadros/cesta/1.png'),
+            galeria: [
+                        require('@/assets/cuadros/cesta/1.png'),
+                    ],
          }
-    ],
-
-        }
-    },
-    methods:{
-        home(){
-            this.$router.push('/')
-        },
-        anterior(){
-            this.$router.go(-1)
-        },
-        siguiente(){
-                this.$router.go(1)
-            }
-        }
+        ],//cuadros
+    }),//data
      
-    }
+}//export default
 </script>
